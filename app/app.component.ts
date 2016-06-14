8e595f67a6f718d65745daac8c04c02ef99cd319
Michael O'Keefe <mokeefe@corp.validar.com>
2016-06-14 15:40:53 -0700

    displays contents of JSON objects from specified GET url (test url currently). JSON display is not well formatted. errors are printed if no JSON object exists

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,42 @@
-import {Component} from '@angular/core';
+import { Component }        from '@angular/core';
+import { HTTP_PROVIDERS, Response }  from '@angular/http';
+import { Observable }       from 'rxjs/Observable';
+import { Subject }          from 'rxjs/Subject';
+import { AppService }       from './app.service';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>Event Insight Test</h1>
+    <ul>
+      {{item}}
+    </ul>
+    <ul>
+    Input GET method:
+    <input #search />
+    <button (click)="getPosts(search.value); search.value=''">
+      Search
+    </button>
+    <div class="error" *ngIf="errorMessage">{{errorMessage}}</div>
+  `,
+  providers: [HTTP_PROVIDERS, AppService]
 })
-export class AppComponent { }
+
+
+export class AppComponent {
+  errorMessage: string;
+  public item: Response;
+  mode = 'Observable';
+
+  constructor (private appService: AppService) { }
+
+  
+
+  getPosts(searchVal: string): void {
+    this.appService.getPosts(searchVal) 
+                    .subscribe(
+                      newItem => this.item = newItem,
+                      error => this.errorMessage = <any>error);
+  } 
+}
+
